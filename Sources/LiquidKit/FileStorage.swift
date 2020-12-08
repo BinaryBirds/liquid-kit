15500d9abd6668cbc0a07e894aed3a277bf2a15a
Tibor Bödecs <mail.tib@gmail.com>
2020-12-08 16:00:56 +0100

    copy & move api

--- Sources/LiquidKit/FileStorage.swift
+++ Sources/LiquidKit/FileStorage.swift
@@ -15,7 +15,13 @@ public protocol FileStorage {
 
     /// uploads the data under the given key
     func upload(key: String, data: Data) -> EventLoopFuture<String>
+
+    /// copy a file using a source key to a given destination key
+    func copy(key: String, to: String) -> EventLoopFuture<String>
     
+    /// move a file using a source key to a given destination key
+    func move(key: String, to: String) -> EventLoopFuture<String>
+
     /// create a new directory for a given key
     func createDirectory(key: String) -> EventLoopFuture<Void>
     
