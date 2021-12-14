48cda11e866ffe1de98f862406819f8f5ccfb97f
Tibor Bödecs <mail.tib@gmail.com>
2021-12-14 14:33:31 +0100

    dep updates

--- Package.swift
+++ Package.swift
@@ -1,16 +1,16 @@
-// swift-tools-version:5.3
+// swift-tools-version:5.5
 import PackageDescription
 
 let package = Package(
     name: "liquid-kit",
     platforms: [
-       .macOS(.v10_15)
+       .macOS(.v12)
     ],
     products: [
         .library(name: "LiquidKit", targets: ["LiquidKit"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/apple/swift-nio.git", from: "2.26.0"),
+        .package(url: "https://github.com/apple/swift-nio.git", from: "2.36.0"),
         .package(url: "https://github.com/apple/swift-log.git", from: "1.4.0"),
     ],
     targets: [
